b0c096cb7fd412a5b86ec3ab7f398fc6f63583b9
marsil1990 <ma.silveram@gmail.com>
2025-12-18 19:05:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,99 @@
+/* ***********************
+ * Require Statements
+ *************************/
+const session = require("express-session");
+// const pool = require("./database/");
+const express = require("express");
+const bodyParser = require("body-parser");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController");
+const utilities = require("./utilities/index");
+const cookieParser = require("cookie-parser");
+
+/* ***********************
+ * Middleware
+ * ************************/
+// app.use(
+//   session({
+//     store: new (require("connect-pg-simple")(session))({
+//       createTableIfMissing: true,
+//       pool,
+//     }),
+//     secret: process.env.SESSION_SECRET,
+//     resave: true,
+//     saveUninitialized: true,
+//     name: "sessionId",
+//   })
+// );
+
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static);
+
+/* ***********************
+ * Index Route
+ *************************/
+
+app.get("/", utilities.handleErrors(baseController.buildHome));
+
+// error de robots
+app.get("/robots.txt", (req, res) => res.status(204).end());
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  //   let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    // nav,
+  });
+});
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`);
+});
